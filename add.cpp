4eb050165c0d83c5542511223a5cb615b154f58b
ssh1305 <shweta.singh@causecode.com>
2016-07-09 14:20:14 +0530

    Minor changes

--- add.cpp
+++ add.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   int a,b
   std::cin >> a >> b;
-  std::cout << a+b << std::endl;
-  std::cout << a-b << std::endl;
+  std::cout << "Addition: " << a+b << std::endl;
+  std::cout << "Subtraction: " << a-b << std::endl;
   return 0;
 }
