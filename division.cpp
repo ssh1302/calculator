b59da652b5de3d73bac3f5df2be1a4a283459820
ssh1305 <shweta.singh@causecode.com>
2016-07-09 14:27:04 +0530

    Division method

---
+++ division.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[]) {
+  int a,b
+  std::cin >> a >> b;
+  std::cout << "Division: " << a/b << std::endl;
+  return 0;
+}
